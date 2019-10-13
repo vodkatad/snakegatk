2397baa6ef3626802209b5d1969fccad56806fca
Elena Grassi <grassi.e@gmail.com>
2019-10-13 13:57:03 +0200

    Helper script to add docker: after each singularity: directive

---
+++ local/src/add_docker.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+my $docker = "FIXME";
+if (scalar(@ARGV) == 1) {
+    $docker = $ARGV[0];
+}
+my $next = "";
+while (<STDIN>) {
+    if ($next ne "") {
+        print $next;
+        $next = "";
+    }
+    if ($_ =~ /^(\s+)singularity:/) {
+        $next = $1 . 'docker: "' . $docker . '"' . "\n";
+    } 
+    print $_;
+}
+# singularity cannot be last line so no last loop issue
