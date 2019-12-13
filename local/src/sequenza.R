9696025abd133b3952c7a1b51105dab74475847b
Elena Grassi <grassi.e@gmail.com>
2019-12-13 19:06:57 +0100

    sequenza and find_matched rewiring

---
+++ local/src/sequenza.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = T)
+sample <- args[1]
+seqz <- args[2]
+outdir <- args[3]
+cores <- as.numeric(args[4])
+library("sequenza")
+
+#extractData <- sequenza.extract(seqz,window = 10e5,min.reads=50,min.reads.normal=20) # def 10e6 40 10, this is sottoriva's our depth is 30x..
+extractData <- sequenza.extract(seqz, min.reads=20, min.reads.normal=10)# parallel = cores) # def 10e6 40 10, this is sottoriva's our depth is 30x..
+#extractData.CP <- sequenza.fit(extractData, segment.filter = 5e6)
+extractData.CP <- sequenza.fit(extractData)#, mc.cores = cores)
+sequenza.results(extractData, extractData.CP, out.dir = outdir ,sample.id = sample)
+#save.image(paste0(sample,"_sequenza.RData"))
