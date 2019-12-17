35f20965284f1a430db676b455b573421927e7e7
Elena Grassi <grassi.e@gmail.com>
2019-12-17 18:07:42 +0100

    json metadata: date formatting and append ing

--- local/src/tsv_to_json_metadata.py
+++ local/src/tsv_to_json_metadata.py
@@ -1,39 +1,49 @@
 import json
 import os
+import datetime
 from argparse import ArgumentParser
 
-# TODO: date format, get a metadata.json file to complete (honestly having the schema here and validating etc seems an overkill)
+# maybe-FIXME: it could be wiser to load an entire excel with all info on two sheets, 
+# load everything then validate vs the schema rather than this "patching" of the
+# manually generated first portion of the json, but honestly not urgent.
 
 def main():
     args = get_args()
     with open(args.input, 'r') as tsv:
         header = tsv.readline().rstrip()
         fields = header.split("\t")
-        with open(args.output, 'w') as json_meta:
+        with open(args.json, 'a') as json_meta:
+            entries = []
             for line in tsv.readlines():
                 line = line.rstrip()
                 values = line.split("\t")
                 entry = { "id": values[2], "LAS_Validation": values[3] == "TRUE", "files": get_files(values[0], args.fastq) }
                 #if len(values) >= 4:
                 for i in range(4, len(values)):
-                    entry[fields[i]] = values[i]
-                json_meta.write(json.dumps(entry) + "\n")        
+                    if fields[i] == "cloning_date":
+                        entry[fields[i]] = str(datetime.datetime.strptime(values[i], '%Y/%m/%d').date())
+                    else:
+                        entry[fields[i]] = values[i]
+                entries.append(entry) # python triiiiicky indenting
+                #json_meta.write(json.dumps(entry) + "\n")     
+            jsons = json.dumps({"samples_map": entries}, indent=4)
+            json_meta.write(jsons.strip('{}'))
+            json_meta.write("}\n")
 
 def get_files(vendor_id, fq_dir):
     return [os.path.join(fq_dir, f) for f in [vendor_id+"S1_L004_R1_001.fastq.gz", vendor_id+"S1_L004_R2_001.fastq.gz"]]
 
 def get_args():
     parser = ArgumentParser(description="From a sample sheet in excel format to the json samples_map for las mdam")
-    parser.add_argument("-o", "--output",
-                        default="metadata.json",
-                        required=True,
-                        help="path and name of output file. Directories must exist.")
     parser.add_argument("-f", "--fastq",
                         default="./fastq/",
                         help="the fastq dir")
     parser.add_argument("-i", "--input",
                         required=True,
                         help="the tsv sample sheet")
+    parser.add_argument("-j", "--json",
+                        required=True,
+                        help="the json to be filled")
     args = parser.parse_args()
     return args
 
