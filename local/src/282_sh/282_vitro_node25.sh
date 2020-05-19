4d7d8346be0d1ce3bbffac17b604ce3ca1787a4d
Elena <grassi.e@gmail.com>
2020-05-19 17:28:41 +0200

    282 conf and at generation

---
+++ local/src/282_sh/282_vitro_node25.sh
@@ -0,0 +1,8 @@
+
+ export RUN_NODE=node25 && snakemake -j 2 --use-docker align/realigned_CRC0282-01-1-B.bam align/realigned_CRC0282-01-1-E.bam &> 282_vitro_node25.sh_align.slog;
+ export RUN_NODE=node25 && snakemake -j 24 --use-docker mutect_paired/CRC0282-01-1-B.pass.vcf.gz mutect_paired/CRC0282-01-1-E.pass.vcf.gz &> 282_vitro_node25.sh_mutect_paired_mice.slog;
+ export RUN_NODE=node25 && snakemake -j 2 --use-docker sequenza/CRC0282-01-1-B sequenza/CRC0282-01-1-E &> 282_vitro_node25.sh_sequenza.slog;
+ export RUN_NODE=node25 && snakemake -j 2 --use-docker fastqc_CRC0282-01-1-B fastqc_CRC0282-01-1-E &> 282_vitro_node25.sh_fastqc.slog;
+ export RUN_NODE=node25 && snakemake -j 2 --use-docker align/CRC0282-01-1-B.bam.flagstat align/CRC0282-01-1-E.bam.flagstat &> 282_vitro_node25.sh_flagstat.slog;
+ export RUN_NODE=node25 && snakemake -j 2 --use-docker align/CRC0282-01-1-B.wgsmetrics align/CRC0282-01-1-E.wgsmetrics &> 282_vitro_node25.sh_wgsmetrics.slog;
+ 
