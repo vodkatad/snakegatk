8bd4f6c463ef92c3e745b0b53abd7ee1a2cccf50
Elena Grassi <grassi.e@gmail.com>
2021-07-23 12:27:59 +0000

    Fixed bug in pcgr-af script and added genes info

--- local/src/AFmatrix_filter_pcgr.R
+++ local/src/AFmatrix_filter_pcgr.R
@@ -5,6 +5,7 @@
 #- for each column open mutect/CRC0322LMX0A02201TUMD13000.pcgr_acmg.grch38.snvs_indels.tiers.tsv, keep only TIER 3, put 0 for other muts
 #- remove rows with all 0
 
+# produces parallel matrix with genes level info
 
 # TODO get 'mutect' as a parameter from rule to work also for paired mutect
 library(getopt)
@@ -12,38 +13,48 @@ library(getopt)
 opts <- matrix(c(
   'help', 'h', 0, 'logical',
   'merged_af', 'm', 1, 'character',
+  'output_gene', 'g', 1, 'character',
   'output_af', 'o', 1, 'character'), ncol=4, byrow=TRUE)
 opt <- getopt(opts)
 
-if (!is.null(opt$help) | is.null(opt$merged_af) | is.null(opt$output_af)) {
+if (!is.null(opt$help) | is.null(opt$merged_af) | is.null(opt$output_af) | is.null(opt$output_gene)) {
   cat(getopt(opts, usage=TRUE))
-  stop('-m/-o are mandatory arguments!')
+  stop('-m/-o/-g are mandatory arguments!')
 }
 
 af_table_f <- opt$merged_af
 af_table_out_f <- opt$output_af
+genes_table_out_f <- opt$output_gene
 
 af <- read.table(af_table_f, sep="\t", header=TRUE, row.names = 1)
-
+genes <- as.data.frame(matrix('', nrow=nrow(af), ncol=1), stringsAsFactors=FALSE)
 samples <- colnames(af)
 samples <- gsub('.','-', samples, fixed=TRUE) # so sad, for CRC0282-
+colnames(genes) <- 'gene'
+rownames(genes) <- rownames(af)
+
 
 load_filter_pcgr <- function(sample) {
   d <- read.table(paste0('mutect/', sample, '.pcgr_acmg.grch38.snvs_indels.tiers.tsv'), sep="\t", header=T, stringsAsFactors = FALSE, quote='')
   d$id <- paste0(d$CHROM, ":", d$POS, ":", d$REF, ":", d$ALT)
   W_TIERS <- c('TIER 1', 'TIER 2', 'TIER 3')
   d <- d[d$TIER %in% W_TIERS,]
-  c(paste0('chr', d$id))
+  list(ids=paste0('chr', d$id), genes=d$SYMBOL)
 }
-
-save.image('pippo.Rdata')
+#save.image('pippo.Rdata')
 for (i in seq(1, length(samples))) {
   keep <- load_filter_pcgr(samples[i])
-  af[rownames(af) %in% keep, i] <- 0
+  keepid <- intersect(rownames(af),keep[['ids']]) # our af matrix has out of target variants filtered, pcgr do not
+  igenes <- keep[['genes']]
+  igenes <- igenes[keep[['ids']] %in% keepid]
+  af[!rownames(af) %in% keepid, i] <- 0
+  genes[rownames(genes) %in% keepid,'gene'] <- igenes
 }
 
 r <- rowSums(af) == 0
 print(table(r))
 af <- af[!r,]
+genes <- genes[!r,, drop=FALSE]
 
 write.table(af, file=af_table_out_f, sep="\t", quote=FALSE)
+write.table(genes, file=genes_table_out_f, sep="\t", quote=FALSE)
