8bf9e79814d185275ea1825a23793811a8c3e716
Elena Grassi <grassi.e@gmail.com>
2021-12-28 18:06:45 +0100

    Added long format to pcgr annotation of AF

--- local/src/AFmatrix_filter_pcgr.R
+++ local/src/AFmatrix_filter_pcgr.R
@@ -1,5 +1,5 @@
 #!/opt/R/R-3.6.3/bin/Rscript
-
+library(reshape)
 # steps for targeted annotation:
 #- load merged.table_nomultiallele
 #- for each column open mutect/CRC0322LMX0A02201TUMD13000.pcgr_acmg.grch38.snvs_indels.tiers.tsv, keep only TIER 3, put 0 for other muts
@@ -14,17 +14,22 @@ opts <- matrix(c(
   'help', 'h', 0, 'logical',
   'merged_af', 'm', 1, 'character',
   'output_gene', 'g', 1, 'character',
+  'output_long', 'l', 1, 'character',
+  'wanted_tiers', 'w', 1, 'character',
   'output_af', 'o', 1, 'character'), ncol=4, byrow=TRUE)
 opt <- getopt(opts)
 
-if (!is.null(opt$help) | is.null(opt$merged_af) | is.null(opt$output_af) | is.null(opt$output_gene)) {
+if (!is.null(opt$help) | is.null(opt$merged_af) | is.null(opt$output_af) | is.null(opt$output_gene) | is.null(opt$output_long) |  is.null(opt$wanted_tiers)) {
   cat(getopt(opts, usage=TRUE))
-  stop('-m/-o/-g are mandatory arguments!')
+  stop('-m/-g/-l/-w/-o are mandatory arguments!')
 }
+
+W_TIERS <- unlist(strsplit(opt$wanted_tiers, ',', fixed=TRUE))
 
 af_table_f <- opt$merged_af
 af_table_out_f <- opt$output_af
 genes_table_out_f <- opt$output_gene
+long_af_genes <- opt$output_long
 
 af <- read.table(af_table_f, sep="\t", header=TRUE, row.names = 1)
 #genes <- as.data.frame(matrix('', nrow=nrow(af), ncol=1), stringsAsFactors=FALSE)
@@ -38,11 +43,11 @@ samples <- gsub('.','-', samples, fixed=TRUE) # so sad, for CRC0282-
 load_filter_pcgr <- function(sample) {
   d <- read.table(paste0('mutect/', sample, '.pcgr_acmg.grch38.snvs_indels.tiers.tsv'), sep="\t", header=T, stringsAsFactors = FALSE, quote='')
   d$id <- paste0(d$CHROM, ":", d$POS, ":", d$REF, ":", d$ALT)
-  W_TIERS <- c('TIER 1', 'TIER 2', 'TIER 3')
+  #W_TIERS <- c('TIER 1', 'TIER 2', 'TIER 3')
   d <- d[d$TIER %in% W_TIERS,]
-  list(ids=paste0('chr', d$id), genes=d$SYMBOL)
+  list(ids=paste0('chr', d$id), genes=d$SYMBOL, cds=d$CDS_CHANGE)
 }
-save.image('pippo.Rdata')
+
 keepids <- c()
 for (i in seq(1, length(samples))) {
   keep <- load_filter_pcgr(samples[i])
@@ -56,7 +61,9 @@ for (i in seq(1, length(samples))) {
   #[1] 3 2
   igenes <- keep[['genes']]
   igenes <- igenes[keep[['ids']] %in% keepid]
-  kg <- data.frame(symbol=keepid, genes=igenes) # is order mantained this way? TODO check for an example where we remove smt
+  icds <- keep[['cds']]
+  icds <- icds[keep[['ids']] %in% keepid]
+  kg <- data.frame(symbol=keepid, genes=igenes, cds=icds) # is order mantained this way? TODO check for an example where we remove smt
   
   #af[!rownames(af) %in% keepid, i] <- 0 # no! # YES! we need to remove the variants not in the right tiers! TODO
   
@@ -68,7 +75,6 @@ for (i in seq(1, length(samples))) {
     genes <- kg
   }
 }
-save.image('pippo.Rdata')
 genes <- unique(genes) # what's the difference
 #genes <- genes[genes$symbol %in% unique(genes$symbol),, drop=FALSE]
 print(length(unique(keepids)))
@@ -78,7 +84,18 @@ r <- rowSums(af) == 0
 print(table(r))
 af <- af[!r,]
 #genes <- genes[!r,, drop=FALSE]
-save.image('pippa.Rdata')
 
 write.table(af, file=af_table_out_f, sep="\t", quote=FALSE)
 write.table(genes, file=genes_table_out_f, sep="\t", quote=FALSE, row.names = FALSE)
+
+print('Checking if filtering of af based on TIERs and muts info based on AF worked')
+stopifnot(nrow(genes) == nrow(af))
+save.image('pippo.Rdata')
+m <- merge(af, genes, by.x='row.names', by.y='symbol')
+stopifnot(nrow(genes) == nrow(m))
+
+cols <- colnames(m)
+cols <- cols[grepl('CRC', cols, fixed=TRUE)]
+long <- melt(m, id.vars=c('Row.names', 'genes', 'cds'), measure.vars=cols)
+colnames(long) <- c('mut_id','gene','cds','lgenealogy','af')
+write.table(long, file=long_af_genes, sep="\t", quote=FALSE)
