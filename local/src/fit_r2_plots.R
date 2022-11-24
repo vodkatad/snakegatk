f7c6486d939eb6a2c98483befbba37fa691f20b3
Elena Grassi <grassi.e@gmail.com>
2022-11-24 11:28:12 +0100

    Summarized plots for fits

--- local/src/fit_r2_plots.R
+++ local/src/fit_r2_plots.R
@@ -7,9 +7,9 @@ log_f <- snakemake@log[["log"]]
 r2_thr <- as.numeric(snakemake@params[['r2_thr']])
 subcl_thr <- as.numeric(snakemake@params[['subcl_thr']])
 
-r2_thr <- 0.9
-subcl_thr <- 10
-fit_r2_f <- '/mnt/trcanmed/snaketree/prj/snakegatk/dataset/Pri_Mets_godot/bestbet_0.05_0.2.tsv'
+#r2_thr <- 0.9
+#subcl_thr <- 10
+#fit_r2_f <- '/mnt/trcanmed/snaketree/prj/snakegatk/dataset/Pri_Mets_godot/bestbet_0.05_0.2.tsv'
 
 printf <- function(...) cat(sprintf(...))
 
@@ -20,22 +20,29 @@ fit_r2$smodel <- substr(rownames(fit_r2), 0, 7)
 fit_r2$mp <- substr(rownames(fit_r2), 8, 10)
 fit_r2$mp <- factor(fit_r2$mp, levels=c('PRX', 'LMX'))
 
+ggplot(data=fit_r2, aes(x=r, fill=mp))+
+  geom_histogram(position="dodge", bins=25)+
+  theme_bw()+theme(text=element_text(size = 18))+ 
+  scale_fill_manual(values=c('#adacac', '#595959'))
+ggsave(r2_plot_f)
 
 fit_keep <- fit_r2[fit_r2$r > 0.90 & fit_r2$subcl > 10,]
 
 pairs <- as.data.frame(table(fit_keep$smodel))
 with_pair <- fit_keep[fit_keep$smodel %in% pairs[pairs$Freq == 2,'Var1'],]
 
-sink(log)
-printf('Total\t%d\nFiltered\t%d\nPairs\t%d\n', nrow(fit_r2), nrow(fit_keep), length(unique(fit_keep$smodel)))
+sink(log_f)
+printf('Total\t%d\nFiltered\t%d\nPairs\t%d\n', nrow(fit_r2), nrow(fit_keep), length(unique(with_pair$smodel)))
 sink()
 
+# order by delta
 lar <- function(x, data) {
   sub <- data[data$smodel == x,]
   sub[sub$mp=="LMX",'intercept'] - sub[sub$mp=="PRX",'intercept']
 }
 
 
+# order by absolute slope of met
 #meslo <- function(x, data) {
 #  sub <- data[data$smodel == x,]
 #  sub[sub$mp=="LMX",'intercept']
@@ -59,6 +66,17 @@ ggsave(delta_plot_f)
 
 met <- d3[d3$mp == "LMX",]
 pri <- d3[d3$mp == "PRX",]
-all(met$smodel==pri$smodel)
-t.test(met$intercept, pri$intercept, alternative="greater", paired=TRUE)
-wilcox.test(met$intercept, pri$intercept, alternative="greater", paired=TRUE)
+if (!all(met$smodel==pri$smodel)) {
+  stop('llama! Qualquadra non cosa in pri-met pairs')
+}
+ti <- t.test(met$intercept, pri$intercept, alternative="greater", paired=TRUE)
+wi <- wilcox.test(met$intercept, pri$intercept, alternative="greater", paired=TRUE)
+
+sink(log_f, append=TRUE)
+printf('ttest\t%.4f\nwilcox\t%.4f\nPri_avg\t%.4f\nMet_avg\t%.4f\nMet_larger\t%.1f\n', 
+       ti$p.value, wi$p.value, mean(pri$intercept), mean(met$intercept), sum(d3$delta > 0)/2)
+summary(fit_r2[fit_r2$mp == "PRX", 'r'])
+summary(fit_r2[fit_r2$mp == "LMX", 'r'])
+sink()
+
+
