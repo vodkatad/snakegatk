c369c515a4baebb01355efd1410b597b76ed70e8
Elena Grassi <grassi.e@gmail.com>
2023-12-06 09:41:08 +0100

    LOH+mixed

--- local/src/pyclone_in.R
+++ local/src/pyclone_in.R
@@ -18,7 +18,16 @@ pyclone_in <- data.frame(mutation_id=rownames(AD), sample_id=sample, ref_counts=
                          minor_cn=B, normal_cn=2, tumour_content=1, error_rate=0.001)
 
 pyclone_in <- pyclone_in[!is.na(pyclone_in$major_cn) & !is.na(pyclone_in$minor_cn),]
+pyclone_in <- pyclone_in[pyclone_in$minor_cn!=0,] # remove LOH
+#pyclone_in <- pyclone_in[!grepl('chrY', pyclone_in$mutation_id),]
+#pyclone_in <- pyclone_in[!grepl('chrX', pyclone_in$mutation_id),]
 pyclone_in[grepl('chrY', pyclone_in$mutation_id), 'normal_cn'] <- 1
+#pyclone_in$tot <- pyclone_in$ref_counts+ pyclone_in$alt_counts
+#pyclone_in <- pyclone_in[pyclone_in$tot > 50,]
+#pyclone_in$tot <- NULL
 #try with a thr on VAF
-pyclone_in <- pyclone_in[(pyclone_in$alt / (pyclone_in$ref+pyclone_in$alt)) > 0.05,]
+#pyclone_in <- pyclone_in[(pyclone_in$alt / (pyclone_in$ref+pyclone_in$alt)) > 0.05,]
+pyclone_in <- pyclone_in[(pyclone_in$minor_cn+pyclone_in$major_cn) < 5,]
 write.table(pyclone_in, file=out_f, quote=FALSE, sep="\t")
+
+save.image('beh.Rdata')
